c2a224dbb0484d9b13c84a659fa1495e8c2d4af9
Gaoqiao <1979836005@qq.com>
2024-12-17 12:12:09 +0800

    JavaScript

---
+++ front/js/index.js
@@ -0,0 +1,24 @@
+//打印hello world
+console.log('hello world');
+
+//var(全局变量：不推荐使用) let, const
+let age = 31;
+console.log(age);
+
+//弹窗
+alert('你好呀');
+
+let result = confirm('你听说过高桥凉介吗');
+console.log(result);
+
+
+let a = prompt('请你输入一点东西');
+console.log(a);
+
+const res = 1;
+
+document.write('高桥凉介');
+
+console.error('这是一个错误')
+
+console.warn('这是一个警告')
---
+++ front/js/swap.js
@@ -0,0 +1,15 @@
+// let a, b;
+// a  = 3, b = 4;
+//
+// let temp = a;
+// a = b;
+// b = temp;
+//
+// console.warn(a);
+// console.warn(b);
+
+let a = 1;
+let A = 2;
+
+console.log(a);
+console.log(A);
